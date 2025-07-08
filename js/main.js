94dd5e1b0ddd79857b88ad39d02237d8d06b1734
Surendranp <surayrk315@@example.com>
2025-07-08 17:58:56 +0530

    test-1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-   const bundles = document.querySelectorAll('.bundle');
+        const bundles = document.querySelectorAll('.bundle');
         bundles.forEach(bundle => {
             bundle.addEventListener('click', () => {
                 bundles.forEach(b => b.classList.remove('active'));
